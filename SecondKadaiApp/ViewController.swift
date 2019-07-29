c76f2cef6f3018eac5ea8cf40f07646303b2d4e2
u-sk <u.eggplant@gmail.com>
2019-07-29 21:04:20 +0900

    textFieldに入力された文字列を表示できるように修正しました。

--- SecondKadaiApp/ViewController.swift
+++ SecondKadaiApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,8 +20,9 @@ class ViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // segueから遷移先のResultViewControllerを取得する
         let resultViewController:ResultViewController = segue.destination as! ResultViewController
+        
         // 遷移先のResultViewControllerで宣言しているx, yに値を代入して渡す
-        resultViewController.name = "太郎"
+        resultViewController.name = textField.text!
     }
 
     @IBAction func unwind(_ segue: UIStoryboardSegue) {
